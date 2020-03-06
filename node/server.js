b2618bf62274f329109ff0e0aa400028f67eeebf
Hazrat Ali <iloveyii@yahoo.com>
2020-03-06 12:37:54 +0100

    Added get games

--- node/server.js
+++ node/server.js
@@ -4,6 +4,43 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const Pool = require('pg').Pool
+const Sequelize = require('sequelize')
+let conString = "postgres://ngnfhein:RPw8GlvfDa26UcZmyG82trgJsgOBFyPZ@drona.db.elephantsql.com:5432/ngnfhein";
+conString = "postgres://postgres:root1@3@localhost:5432/sports";
+const sequelize = new Sequelize(conString);
+const Model = Sequelize.Model;
+
+class Game extends Model {
+}
+
+// Test connection
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+        // Find all games
+        Game.init({
+            awayName: {type: Sequelize.STRING, allowNull: true},
+            homeName: Sequelize.STRING,
+            createdAt: Sequelize.DATE,
+            group: Sequelize.STRING,
+            id: {type: Sequelize.INTEGER, primaryKey: true},
+            name: Sequelize.STRING,
+            sport: Sequelize.STRING,
+            country: Sequelize.STRING,
+            state: Sequelize.STRING
+        }, {
+            sequelize,
+            modelName: "Games"
+        })
+        //Game.findAll().then(games => {
+            // console.log("All games:", JSON.stringify(games, null, 4));
+        //});
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 
 const pool = new Pool({
     url: 'postgres://ngnfhein:RPw8Gl...@drona.db.elephantsql.com:5432/ngnfhein',
@@ -15,7 +52,6 @@ const pool = new Pool({
 });
 
 var pg = require('pg');
-var conString = "postgres://ngnfhein:RPw8GlvfDa26UcZmyG82trgJsgOBFyPZ@drona.db.elephantsql.com:5432/ngnfhein";
 
 var client = new pg.Client(conString);
 client.connect();
@@ -33,12 +69,12 @@ app.use(
 );
 
 
-app.get('/',  (req, res)=> {
+app.get('/', (req, res) => {
     const paths = app._router.stack
     const defined_paths = []
     // console.log(Object.keys(paths))
     paths.forEach(p => {
-        if(p.route && p.route.path) {
+        if (p.route && p.route.path) {
             defined_paths.push({
                 path: p.route.path,
                 method: p.route.stack[0].method
@@ -47,13 +83,18 @@ app.get('/',  (req, res)=> {
     });
     return res.json({paths: defined_paths})
 });
-app.get('/api/v1/games',  (req, res)=> {
-    client.query('SELECT * FROM "public"."Games"  ORDER BY id ASC', (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json(results.rows)
-    })
+app.get('/api/v1/games', (req, res) => {
+    console.log('GET api/v1/games')
+
+    const games = async () => {
+        const rows = await
+            Game.findAll().then(games => {
+                console.log("All games:", JSON.stringify(games.length, null, 4));
+                return games
+            });
+        return res.status(200).json(rows)
+    };
+    games()
 });
 /**
  "awayName": "Panthrakikos Komotini",
@@ -64,7 +105,7 @@ app.get('/api/v1/games',  (req, res)=> {
  "country": "ENGLAND",
  "state": "STARTED"
  */
-app.post('/api/v1/games',  (req, res)=> {
+app.post('/api/v1/games', (req, res) => {
     const {awayName, group, homeName, name, sport, country, state} = req.body;
     client.query(
         `INSERT INTO 
@@ -76,8 +117,8 @@ app.post('/api/v1/games',  (req, res)=> {
     res.status(201).json({game: 1});
 });
 
-app.delete('/api/v1/games/:id',  (req, res)=> res.json({game: 1}));
-app.put('/api/v1/games/:id',  (req, res)=> res.json({game: 1}));
+app.delete('/api/v1/games/:id', (req, res) => res.json({game: 1}));
+app.put('/api/v1/games/:id', (req, res) => res.json({game: 1}));
 
 
 app.listen(8080, () => console.log('Server started on port ' + 8080));
