72edc6bcb996695549d7ff7311e5c0207c10d08c
Hazrat Ali <iloveyii@yahoo.com>
2020-03-06 22:32:02 +0100

    Find one

--- node/server.js
+++ node/server.js
@@ -63,15 +63,16 @@ app.get('/api/v1/games/:id', (req, res) => {
     console.log('GET api/v1/games/:id')
     const game = GameModel(sequelize, Sequelize)
     const id = req.params.id ? req.params.id : null
-    game.findAll({id : id}).then(games => res.status(200).json(games))
+    console.log('ID: ', id)
+    game.findAll({where: {id: id}}).then(games => res.status(200).json(games))
 });
 
 app.post('/api/v1/games', (req, res) => {
     const {awayName, group, homeName, name, sport, country, state} = req.body;
     const game = GameModel(sequelize, Sequelize)
     game.create({
-        awayName:awayName, group:group, homeName:homeName, name:name, sport:sport, country:country, state:state
-    }).then( r => res.status(201).json({game: r}));
+        awayName: awayName, group: group, homeName: homeName, name: name, sport: sport, country: country, state: state
+    }).then(r => res.status(201).json({game: r}));
 });
 
 app.delete('/api/v1/games/:id', (req, res) => res.json({game: 1}));
